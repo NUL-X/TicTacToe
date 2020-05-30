a1ac5804b600547686fe2af529a61ec9319cbeda
Youssef-AH <qpinoza@gmail.com>
2020-05-30 16:41:55 +0100

    Created Structures.cs

--- src/Entities/Structures.cs
+++ src/Entities/Structures.cs
@@ -1,7 +1,34 @@
 ﻿namespace TicTacToe.Entities
 {
-    public enum Structures
+    public static class Structures
     {
+        #region Structures
+
+        // type of move user played
+        public enum MoveType
+        {
+            None = 0,
+            Ball = 1,
+            Cross = 10
+        }
         
+        // AI difficulty
+        public enum AIDifficulty
+        {
+            Easy = 0,
+            Average = 1,
+            Hard = 2
+        }
+        
+        // Indicates which line to draw when player wins
+        public enum LineType
+        {
+            Vertical = 0,
+            Horizontal = 1,
+            RightDiagonal = 2,
+            LeftDiagonal = 3
+        }
+
+        #endregion Structures
     }
 }
