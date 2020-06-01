ec809aa2e17b60f94b9b61619a5faa16d68f5260
Mohamed MERZAK <MERZAK_MOHAMED@emsi-edu.ma>
2020-06-01 13:49:59 +0100

    Added TODO: @Yassine-Ag

--- src/Forms/Main.cs
+++ src/Forms/Main.cs
@@ -1,12 +1,31 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace TicTacToe.Forms
 {
     public partial class Main : Form
     {
+        public bool isConnected = false;
+        public bool isClient = false;
         public Main()
         {
             InitializeComponent();
         }
+        
+        public void displayMatrix(){
+            // TODO: @Yassine-Ag Display a stored matrix from the saved games list 
+        }
+
+        public void MakeMove(int x, int y)
+        {
+            // TODO: @Yassine-Ag Store in matrix then display to the game grid
+        }
+        
+        public void RestartGame(){}
+        
+        public void SetStatusMessage(string msg){}
+        
+        public void mnDisconnect_Click(object sender, EventArgs args){}
+        
     }
 }
